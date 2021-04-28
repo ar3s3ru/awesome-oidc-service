e390ecc1f4b800317aff8f9bf722c42d7fb7a0f0
Athavan kanapuli <akan@hellofresh.com>
2021-04-28 15:48:39 +0200

    Initialize tracing in the http server

--- src/bin/oidc-service-api/main.rs
+++ src/bin/oidc-service-api/main.rs
@@ -1,9 +1,14 @@
 mod api;
 use actix_web::{get, App, HttpServer, Responder};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let fmt_layer = tracing_subscriber::fmt::layer().json();
+    tracing_subscriber::registry().with(fmt_layer).try_init();
+
     HttpServer::new(|| App::new().service(api::health))
         .bind("127.0.0.1:8080")?
         .run()
