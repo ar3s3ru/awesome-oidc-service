6e0d330ca2a6fd56a2b6ee5689d69f954e9e02ce
Herbert Henrique <hhenr@hellofresh.com>
2021-04-28 16:08:17 +0200

    use anyhow for errors

--- src/bin/oidc-service-api/main.rs
+++ src/bin/oidc-service-api/main.rs
@@ -1,16 +1,17 @@
 mod api;
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{App, HttpServer};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
-
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     let fmt_layer = tracing_subscriber::fmt::layer().json();
-    tracing_subscriber::registry().with(fmt_layer).try_init();
+    tracing_subscriber::registry().with(fmt_layer).try_init()?;
 
     HttpServer::new(|| App::new().service(api::health))
-        .bind("127.0.0.1:8080")?
+        .bind("127.0.0.1:8081")?
         .run()
-        .await
+        .await?;
+
+    Ok(())
 }
