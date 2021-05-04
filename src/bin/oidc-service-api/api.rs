84520b3aaeea97234f2b4311f3d586e2622e627b
Athavan kanapuli <akan@hellofresh.com>
2021-05-04 14:27:21 +0200

    Add API for post users

--- src/bin/oidc-service-api/api.rs
+++ src/bin/oidc-service-api/api.rs
@@ -1,6 +1,20 @@
-use actix_web::{get, Responder};
+use actix_web::{get, post, web, HttpResponse, Responder};
+use serde::Deserialize;
 
 #[get("/health")]
 async fn health() -> impl Responder {
     "I am ready"
+}
+
+#[derive(Deserialize, Debug)]
+struct PostUsersRequest {
+    email: String,
+    firstName: String,
+    lastName: String,
+}
+
+#[post("/users")]
+async fn post_users(user: web::Json<PostUsersRequest>) -> impl Responder {
+    tracing::debug!(?user);
+    HttpResponse::NotImplemented()
 }
--- src/bin/oidc-service-api/main.rs
+++ src/bin/oidc-service-api/main.rs
@@ -8,7 +8,7 @@ async fn main() -> anyhow::Result<()> {
     let fmt_layer = tracing_subscriber::fmt::layer().json();
     tracing_subscriber::registry().with(fmt_layer).try_init()?;
 
-    HttpServer::new(|| App::new().service(api::health))
+    HttpServer::new(|| App::new().service(api::health).service(api::post_users))
         .bind("127.0.0.1:8081")?
         .run()
         .await?;
